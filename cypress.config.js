53f2f906642fc28ed9defd8f0d9c5a86197beb3a
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-09 00:39:16 +0530

    8

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      sspecPattern: "cypress/tests/*.cy.js"
     },
-    sspecPattern: "cypress/tests/*.cy.js",
   },
 
   component: {
